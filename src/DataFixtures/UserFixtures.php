3e855379ad7fd1a08f82e29160cec224b6d755fe
aleksandrvergunov <vergunov.123456@mail.ru>
2022-06-01 19:31:32 +0700

    feat: added UserFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    const USERS_LIST = [
+        array(
+            'username' => 'admin',
+            'roles' => ["ROLE_ADMIN"]   
+        ),
+        array(
+            'username' => 'user',
+            'roles' => []   
+        ),
+    ];
+
+    private $passwordHasher;
+
+    public function __construct(
+        UserPasswordHasherInterface $passwordHasher
+    )
+    {
+        $this->passwordHasher = $passwordHasher;    
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::USERS_LIST as $user) {
+            $newUser = new User;
+            $newUser->setEmail($user['username']);
+            $newUser->setPassword($this->passwordHasher->hashPassword($newUser, $user['username']));
+            
+            if (!empty($user['roles'])) {
+                $newUser->setRoles($user['roles']);
+            }
+
+            $manager->persist($newUser);
+        }
+
+        $manager->flush();
+    }
+}
